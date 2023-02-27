c853a021fef8d3f0287d86526b96a4f286532157
poornimapk <poornima.pk@gmail.com>
2023-02-27 11:14:36 -0800

    Added User table and loaded default values to User Table

--- src/main/scala/db/DBSchema.scala
+++ src/main/scala/db/DBSchema.scala
@@ -5,13 +5,30 @@ import slick.jdbc.H2Profile.api._
 import scala.concurrent.duration._
 import scala.concurrent.Await
 import scala.language.postfixOps
+import models._
 
 object DBSchema {
   /**
    * Load schema and populate sample data withing this Sequence od DBActions
    */
-  val databaseSetup = DBIO.seq(
+  class UsersTable(tag: Tag) extends Table[User](tag, "Users") {
+    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+    def email = column[String]("EMAIL")
+    def firstName = column[String]("FIRSTNAME")
+    def lastName = column[String]("LASTNAME")
 
+    def * = (id, email, firstName, lastName).mapTo[User]
+  }
+
+  val Users = TableQuery[UsersTable]
+
+  val databaseSetup = DBIO.seq(
+    Users.schema.create,
+    Users forceInsertAll Seq(
+      User(1, "sample1@gmail.com", "Sample1FN", "Sample1LN"),
+      User(2, "sample2@gmail.com", "Sample2FN", "Sample2LN"),
+      User(3, "sample3@gmail.com", "Sample3FN", "Sample3LN"),
+    )
   )
 
 
---
+++ src/main/scala/models/package.scala
@@ -0,0 +1,3 @@
+package object models {
+  case class User(id: Int, email: String, firstName: String, lastName: String)
+}
